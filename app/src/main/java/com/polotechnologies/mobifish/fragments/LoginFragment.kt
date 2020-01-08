620befa3c395da6d173a6176ff31df1b26111676
polojerry <polojerry4@gmail.com>
2020-01-08 16:41:16 +0300

    Refactoring to Kotlin-->>User SignUp Done

--- app/src/main/java/com/polotechnologies/mobifish/fragments/LoginFragment.kt
+++ app/src/main/java/com/polotechnologies/mobifish/fragments/LoginFragment.kt
@@ -1,15 +1,22 @@
 package com.polotechnologies.mobifish.fragments
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.AuthUI.IdpConfig.PhoneBuilder
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.FirebaseUser
 import com.polotechnologies.mobifish.R
 import com.polotechnologies.mobifish.databinding.FragmentLoginBinding
 
@@ -18,21 +25,22 @@ import com.polotechnologies.mobifish.databinding.FragmentLoginBinding
  */
 class LoginFragment : Fragment() {
 
-    private lateinit var mBinding : FragmentLoginBinding
+    private lateinit var mBinding: FragmentLoginBinding
     private lateinit var mAuth: FirebaseAuth
+
+    private val RC_PHONE_SIGNUP = 101
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         mAuth = FirebaseAuth.getInstance()
 
         mBinding.btnLogin.setOnClickListener {
             loginUser()
         }
         mBinding.btnSignUp.setOnClickListener {
-            signUpUser()
+            signUpFlow(it)
         }
-
         return mBinding.root
     }
 
@@ -40,8 +48,55 @@ class LoginFragment : Fragment() {
 
     }
 
-    private fun signUpUser() {
-        findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+    private fun signUpFlow(view: View) {
+        phoneAuth()
     }
 
+    private fun phoneAuth() {
+        // Choose authentication providers
+        val providers = listOf(
+                PhoneBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_PHONE_SIGNUP)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            RC_PHONE_SIGNUP -> signUpUser(resultCode, data)
+        }
+
+    }
+
+    private fun signUpUser(resultCode: Int, data: Intent?) {
+        val response = IdpResponse.fromResultIntent(data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            val firebaseUser = FirebaseAuth.getInstance().currentUser ?: return
+            val action = LoginFragmentDirections.actionLoginFragmentToSignUpFragment(firebaseUser.phoneNumber.toString())
+            findNavController().navigate(action)
+
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            if ((response?.error?.errorCode ?: 0) == ErrorCodes.NO_NETWORK) {
+                printMessage("No Network")
+            } else {
+                printMessage("Cancelled by User")
+            }
+
+        }
+
+    }
+
+    private fun printMessage(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+    }
 }
--- app/src/main/java/com/polotechnologies/mobifish/fragments/SignUpFragment.kt
+++ app/src/main/java/com/polotechnologies/mobifish/fragments/SignUpFragment.kt
@@ -8,9 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 import com.polotechnologies.mobifish.R
+import com.polotechnologies.mobifish.dataModels.User
 import com.polotechnologies.mobifish.databinding.FragmentSignUpBinding
 
 /**
@@ -20,15 +25,48 @@ class SignUpFragment : Fragment() {
 
     private lateinit var mBinding: FragmentSignUpBinding
     private lateinit var mContext:Context
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mDatabaseReference: DatabaseReference
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_sign_up, container, false)
         mContext = this.context!!
+        mAuth = FirebaseAuth.getInstance()
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
+
+        val phoneNumber = SignUpFragmentArgs.fromBundle(arguments!!).userPhoneNumber
 
         setFilledDropDownMenu()
-        
+
+        mBinding.btnSignUp.setOnClickListener {
+            signUpUser(phoneNumber)
+        }
         return mBinding.root
+    }
+
+    private fun signUpUser(phoneNumber: String) {
+        val fullName = mBinding.etSignUpUserName.text.toString()
+        val userCategory = mBinding.filledExposedDropdownUsers.text.toString()
+        val idNumber = mBinding.etIdNumber.text.toString()
+        val uid = mAuth.currentUser!!.uid
+
+
+        val createdUser = User(
+                fullName,
+                idNumber,
+                userCategory,
+                phoneNumber,
+                uid
+        )
+
+        mDatabaseReference.child(getString(R.string.db_node_users)).child(uid).setValue(createdUser)
+                .addOnSuccessListener {
+                    Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(context,"Failure",Toast.LENGTH_SHORT).show()
+                }
     }
 
     private fun setFilledDropDownMenu() {
