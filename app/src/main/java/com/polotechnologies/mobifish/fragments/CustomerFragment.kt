43eb8f907dafd9d43ca0bba2966ee53249e0b599
polojerry <polojerry4@gmail.com>
2020-01-09 14:44:25 +0300

    Refactoring to Kotlin-->>>Updated Customer and Fisherman homeScreens

--- app/src/main/java/com/polotechnologies/mobifish/fragments/CustomerFragment.kt
+++ app/src/main/java/com/polotechnologies/mobifish/fragments/CustomerFragment.kt
@@ -4,20 +4,50 @@ package com.polotechnologies.mobifish.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 import com.polotechnologies.mobifish.R
+import com.polotechnologies.mobifish.databinding.FragmentCustomerBinding
+import com.polotechnologies.mobifish.databinding.FragmentFisherManBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class CustomerFragment : Fragment() {
+class CustomerFragment : Fragment(), Toolbar.OnMenuItemClickListener {
+    lateinit var mBinding: FragmentCustomerBinding
+    lateinit var mAuth:FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false)
+        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_customer, container, false)
+        mBinding.toolbarCustomer.inflateMenu(R.menu.menu_customer)
+        mBinding.toolbarCustomer.setOnMenuItemClickListener(this)
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+
+
+        return mBinding.root
     }
 
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.actionSignOut->signOutUser()
+        }
+        return true
+    }
+
+
+    private fun signOutUser() {
+        mAuth.signOut()
+        findNavController().navigate(R.id.action_customerFragment_to_loginFragment)
+    }
 
 }
--- app/src/main/java/com/polotechnologies/mobifish/fragments/FisherManFragment.kt
+++ app/src/main/java/com/polotechnologies/mobifish/fragments/FisherManFragment.kt
@@ -2,11 +2,12 @@ package com.polotechnologies.mobifish.fragments
 
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 import com.polotechnologies.mobifish.R
 import com.polotechnologies.mobifish.databinding.FragmentFisherManBinding
@@ -14,15 +15,17 @@ import com.polotechnologies.mobifish.databinding.FragmentFisherManBinding
 /**
  * A simple [Fragment] subclass.
  */
-class FisherManFragment : Fragment() {
+class FisherManFragment : Fragment(), Toolbar.OnMenuItemClickListener {
 
     lateinit var mBinding: FragmentFisherManBinding
+    lateinit var mAuth: FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_fisher_man, container, false)
-
-
+        mBinding.toolbarFisherMan.inflateMenu(R.menu.menu_fisher_man)
+        mBinding.toolbarFisherMan.setOnMenuItemClickListener(this)
+        mAuth = FirebaseAuth.getInstance()
 
 
 
@@ -30,4 +33,15 @@ class FisherManFragment : Fragment() {
     }
 
 
+    private fun signOutUser() {
+        mAuth.signOut()
+        findNavController().navigate(R.id.action_fisherManFragment_to_loginFragment)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.actionSignOut->signOutUser()
+        }
+        return true
+    }
 }
