d7e9b31b4af5aab10b67540fb36c8a4c5a12a218
polojerry <polojerry4@gmail.com>
2020-01-13 15:09:32 +0300

    Refactoring to Kotlin-->>>Selecting image from Gallery Updated

--- app/src/main/java/com/polotechnologies/mobifish/fragments/NewFishFragment.kt
+++ app/src/main/java/com/polotechnologies/mobifish/fragments/NewFishFragment.kt
@@ -1,14 +1,24 @@
 package com.polotechnologies.mobifish.fragments
 
 
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 import com.polotechnologies.mobifish.R
 import com.polotechnologies.mobifish.databinding.FragmentCustomerBinding
@@ -19,7 +29,9 @@ import com.polotechnologies.mobifish.databinding.FragmentNewFishBinding
  */
 class NewFishFragment : Fragment(), View.OnClickListener {
 
+    val REQUEST_IMAGE_GET = 10
     lateinit var mAuth: FirebaseAuth
+    lateinit var mDatabaseReference: DatabaseReference
     lateinit var mBinding: FragmentNewFishBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -28,13 +40,58 @@ class NewFishFragment : Fragment(), View.OnClickListener {
         mBinding.toolbarNewFish.navigationIcon = resources.getDrawable(R.drawable.ic_arrow_back)
         mBinding.toolbarNewFish.setNavigationOnClickListener(this)
 
+        mAuth = FirebaseAuth.getInstance()
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
 
 
+        mBinding.imgFishImage.setOnClickListener{
+            Toast.makeText(context,"Clicked", Toast.LENGTH_SHORT).show()
+            selectImageLocation()
+        }
 
 
         return mBinding.root}
 
+    private fun selectImageLocation() {
+        val imageDialog = AlertDialog.Builder(context!!)
+        imageDialog.setTitle("Image Location")
+        imageDialog.setItems(R.array.image_locations, DialogInterface.OnClickListener { _, which ->
+            when(which){
+                0 -> captureImage()
+                1 -> selectFromGallery()
+            }
+        })
+        imageDialog.show()
+    }
+
+    private fun selectFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(context!!.packageManager) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_GET)
+        }
+    }
+
+    private fun captureImage() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun onClick(v: View?) {
         findNavController().navigate(R.id.action_newFishFragment_to_fisherManFragment)
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == Activity.RESULT_OK) {
+            val thumbnail: Bitmap = data!!.getParcelableExtra("data")
+            val fullPhotoUri: Uri = data.data!!
+            // Do work with photo saved at fullPhotoUri
+
+            Glide.with(this.context!!)
+                    .load(fullPhotoUri)
+                    .into(mBinding.imgFishImage)
+        }
+    }
+
 }
